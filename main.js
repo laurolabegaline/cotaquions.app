829258f7d4aa1114a92e9b8f410f75b598ba49ff
labegrow <laurodiegolabegaline@hotmail.com>
2022-10-10 17:06:08 -0400

    Commit!

--- main.js
+++ main.js
@@ -30,6 +30,26 @@ function attveiculo() {
 	let veiculoSelect = optionValue.value;
 	console.log(veiculoSelect);
 }
+//Formatar CPDF
+function formatarCampo(campoTexto) {
+	if (campoTexto.value.length <= 11) {
+		campoTexto.value = mascaraCpf(campoTexto.value);
+	} else {
+		campoTexto.value = mascaraCnpj(campoTexto.value);
+	}
+}
+function retirarFormatacao(campoTexto) {
+	campoTexto.value = campoTexto.value.replace(/(\.|\/|\-)/g, "");
+}
+function mascaraCpf(valor) {
+	return valor.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, "$1.$2.$3-$4");
+}
+function mascaraCnpj(valor) {
+	return valor.replace(
+		/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g,
+		"$1.$2.$3/$4-$5"
+	);
+}
 
 // KM Cheio
 const priceLD = [14, 7.5, 6, 3.6];
